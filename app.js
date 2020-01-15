2394252f8d273f8b919e4e4945cf086866ffc7d3
taher968 <taherrampurawala5253@gmail.com>
2020-01-15 10:50:47 -0500

    add complete package

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set("view engine","ejs");
+
+app.get("/",function(req,res) {
+    res.render("index");
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
